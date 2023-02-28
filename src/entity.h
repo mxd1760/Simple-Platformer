84ddc899c81df7a93c64998b348dba22e35df76c
Marcus Doucette <Marz3587@gmail.com>
2023-02-28 17:16:46 -0500

    simple changes

--- src/entity.h
+++ src/entity.h
@@ -21,4 +21,20 @@ class Entity {
       display_rect.y=y;
       SDL_QueryTexture(texture,NULL,NULL,&display_rect.w,&display_rect.h);
     }
+    bool operator<(const Entity &rhs){
+      if(this->display_rect.y<rhs.display_rect.y){
+        return true;
+      }else if (this->display_rect.y==rhs.display_rect.y){
+        return this->display_rect.x<rhs.display_rect.x;
+      }else{
+        return false;
+      }
+    }
+    bool operator==(const Entity &rhs){
+      return this->display_rect.x==rhs.display_rect.x &&
+        this->display_rect.y==rhs.display_rect.y &&
+        this->display_rect.w==rhs.display_rect.w &&
+        this->display_rect.h==rhs.display_rect.h &&
+        this->texture==rhs.texture;
+    }
 };
